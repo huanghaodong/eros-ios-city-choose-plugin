caf638d7fc320e7e0bdb56337866857b46ee1019
huanghaodong <534634672@qq.com>
2018-11-27 11:07:19 +0800

    update README and demo

--- demo/cityPicker.vue
+++ demo/cityPicker.vue
@@ -11,21 +11,35 @@ export default {
         return {}
     },
     methods: {
-       choosecity.open({
-          setSubmitText:"你好",
-          setTitleText:"哈哈",
-          setCancelText:"还行",
-          setSubmitColor:"#FF2D4B",
-          setTitleColor:"#FF2D4B",
-          setCancelColor:"#FF2D4B",
-          setTitleSize:10,
-          setSubCalSize:10
-          },(city)=>{
-          this.$notice.toast({
-            message: city,
-            duration: 1
-          })
-         });
+      choosecity.open({
+      setSubmitText:'确定',//确定按钮文字
+      setCancelText:'取消',//取消按钮文字
+      setTitleText:'地区选择',//标题文字
+      setSubCalSize:14,//确定取消按钮文字大小
+      setTitleSize:14,//标题文字大小
+      setTitleColor:'#000000',//标题文字颜色
+      setSubmitColor:'#000000',//确定文字颜色
+      setCancelColor:'#000000',//取消文字颜色
+      addressType:'custom',//'custom':自定义地区数据，在如下address中定义  'defalut':使用自带默认数据
+      address:[{
+        name:"重庆",
+        code:"12",
+        city:[
+          {
+            name:"重庆区县",
+            code:"13",
+            area:[
+              {
+                name:"彭水",
+                code:"14"
+              }
+            ]
+          }
+        ]
+      }],//自定义的数据，数据格式必须与该格式一致
+    },(place)=>{
+
+    })
     }
 }
 </script>
